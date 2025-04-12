4b798faa29e910c0623e95d7911b26dbe9aca866
Lucas Gabriel <gomesborgesbiel@gmail.com>
2025-04-11 22:55:54 -0300

    finalizacao

--- menu.js
+++ menu.js
@@ -1,22 +1,21 @@
+// menu.js
+
 let Abrirmenu = document.getElementById('menu-abrir');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
-let fecharMenu = document.querySelector('.menu-fechar'); // Seleciona o ícone de fechar
+let fecharMenu = document.getElementById('menu-fechar');
 
-// Abre o menu ao clicar no ícone de menu
+// Abre o menu
 Abrirmenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu');
-    overlay.style.display = 'block'; // Exibe a sobreposição
 });
 
-// Fecha o menu ao clicar no ícone de fechar
+// Fecha o menu ao clicar no botão "X"
 fecharMenu.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
-    overlay.style.display = 'none'; // Esconde a sobreposição
 });
 
-// Fecha o menu quando o usuário clica na sobreposição
+// Fecha o menu ao clicar no fundo escuro
 overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
-    overlay.style.display = 'none'; // Esconde a sobreposição
 });
